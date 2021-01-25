068a9094074bcdb23d0f8ff9dff6a6e20744fe75
Matheus de Assis <assis4002@gmail.com>
2021-01-25 16:55:38 -0300

    UserMatriculationsViewComponent

--- Courses/ViewComponents/UserNameViewComponent.cs
+++ Courses/ViewComponents/UserNameViewComponent.cs
@@ -1,23 +1,28 @@
-﻿using Courses.Models;
+﻿using Courses.AccessData.Interfaces;
+using Courses.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Courses.ViewComponents
 {
-    public class UserNameViewComponent : ViewComponent
+    public class UserMatriculationsViewComponent : ViewComponent
     {
+        private readonly IUserRepository _userRepository;
         private readonly Context _context;
 
-        public UserNameViewComponent(Context context)
+        public UserMatriculationsViewComponent(IUserRepository userRepository, Context context)
         {
+            _userRepository = userRepository;
             _context = context;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(string UserId)
         {
-            string userName = _context.Users.FirstOrDefault(c => c.Id == UserId).UserName;
-            return View("Default",userName);
+            User user = await _userRepository.GetLoggedUser(HttpContext.User);
+
+            return View(await _context.Matriculation.Include(a => a.Course).Where(a => a.UserId == user.Id).ToListAsync());
         }
     }
 }
